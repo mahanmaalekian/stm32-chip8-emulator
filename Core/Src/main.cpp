1473ce48fa989177e031a0ba7d6711c0d92df431
Mahan Maalekian <maalekianmahan@gmail.com>
2025-08-04 14:28:56 -0400

    fix: screen should be updated at 20 Hz (fps)

--- Core/Src/main.cpp
+++ Core/Src/main.cpp
@@ -223,9 +223,9 @@ static void MX_TIM1_Init(void)
 
   /* USER CODE END TIM1_Init 1 */
   htim1.Instance = TIM1;
-  htim1.Init.Prescaler = 3999;
+  htim1.Init.Prescaler = 9999;
   htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim1.Init.Period = 9998;
+  htim1.Init.Period = 419;
   htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim1.Init.RepetitionCounter = 0;
   htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
